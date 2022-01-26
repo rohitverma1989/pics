53f34faa046bea0b933c0ca736b8e9f4f9503dc0
rohitverma1989 <it.rohitverma@gmail.com>
2022-01-26 20:13:36 +0530

    codeCleaningAndFixes/functionalComponentCommenting

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,13 +3,10 @@ import '../styles/App.css'
 
 class EmployeeComponent extends React.Component {
     empList = [];
-    // }
-
-    componentDidUpdate = () => {
-        this.empList= this.props.empList 
-    }
 
     render() {
+        this.empList = this.props.empList;
+
         var emp = this.empList.map((x) => {
             return (
                 <div className='col-6 my-1'>
@@ -25,7 +22,6 @@ class EmployeeComponent extends React.Component {
                     </div>
                 </div>)
         })
-        //   console.log(emp)
         return (<div className='row'>{emp}</div>)
     }
 
@@ -33,5 +29,29 @@ class EmployeeComponent extends React.Component {
         window.open('https://getbootstrap.com/docs/5.1/components/card/', '_blank');
     }
 }
+
+
+// const EmployeeComponent = (props) => {
+//     var emp = props.empList.map((x) => {
+//         return (
+//             <div className='col-6 my-1'>
+//                 <div className="card bg-dark text-light card-shadow">
+//                     <div className="card-header">
+//                         {x.Name}
+//                     </div>
+//                     <div className="card-body">
+//                         <h5 className="card-title">{x.Title}</h5>
+//                         <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+//                         <button onClick={goToBootstrap} className="btn btn-primary">Go To Bootstrap</button>
+//                     </div>
+//                 </div>
+//             </div>)
+//     })
+//     return (<div className='row'>{emp}</div>)
+// }
+
+// const goToBootstrap = () => {
+//     window.open('https://getbootstrap.com/docs/5.1/components/card/', '_blank');
+// }
 
 export default EmployeeComponent;
