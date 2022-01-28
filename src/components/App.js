8ef8ebeab8f5cec21bc9a5001427da2fd6d35950
rohitverma1989 <it.rohitverma@gmail.com>
2022-01-29 01:05:22 +0530

    apiRemovalForTesting

--- src/components/App.js
+++ src/components/App.js
@@ -38,17 +38,24 @@ class AppComponent extends React.Component {
     }
 
     getEmployees = () => {
-        axios.get("http://localhost:5510/api/employee/GetEmployeeList").then(
-            (response) => {
-                console.log("response from web api ");
-                console.log(response);
-                this.setState({ empList: response.data });
-            },
-            (error) => {
-                console.log("error in getting data from api");
-                console.log(error);
-            }
-        )
+        // axios.get("http://localhost:5510/api/employee/GetEmployeeList").then(
+        //     (response) => {
+        //         console.log("response from web api ");
+        //         console.log(response);
+        //         this.setState({ empList: response.data });
+        //     },
+        //     (error) => {
+        //         console.log("error in getting data from api");
+        //         console.log(error);
+        //     }
+        // )
+
+        var emLst = [
+            { Id: '1', Name: 'Rohit', Title: 'S.E' },
+            { Id: '2', Name: 'Saurabh', Title: 'S.E' },
+            { Id: '3', Name: 'Rivanya', Title: 'Kiddo' },
+        ]
+        this.setState({ empList: emLst });
     }
 }
 export default AppComponent;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,11 +3,23 @@ import '../styles/App.css'
 
 class EmployeeComponent extends React.Component {
     empList = [];
+    state = {}
+
+    constructor(props) {
+        super(props);
+        this.cardRef = React.createRef()
+    }
+
+    componentDidMount(){
+        console.log("this is componentDidMount")
+        console.log(this.cardRef)
+    }
+
     render() {
         this.empList = this.props.empList;
         var emp = this.empList.map((x) => {
             return (
-                <div className='col-6 my-1'>
+                <div className='col-6 my-1' key={x.Id} ref={this.cardRef}>
                     <div className="card bg-dark text-light card-shadow">
                         <div className="card-header">
                             {x.Name}
@@ -28,27 +40,4 @@ class EmployeeComponent extends React.Component {
     }
 }
 export default EmployeeComponent;
-
-// const EmployeeComponent = (props) => {
-//     var emp = props.empList.map((x) => {
-//         return (
-//             <div className='col-6 my-1'>
-//                 <div className="card bg-dark text-light card-shadow">
-//                     <div className="card-header">
-//                         {x.Name}
-//                     </div>
-//                     <div className="card-body">
-//                         <h5 className="card-title">{x.Title}</h5>
-//                         <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-//                         <button onClick={goToBootstrap} className="btn btn-primary">Go To Bootstrap</button>
-//                     </div>
-//                 </div>
-//             </div>)
-//     })
-//     return (<div className='row'>{emp}</div>)
-// }
-
-// const goToBootstrap = () => {
-//     window.open('https://getbootstrap.com/docs/5.1/components/card/', '_blank');
-// }
 
